89cd9c763939926fe1107310dbdb8e36aa074859
LeonKattendick <leon.kattendick@gmail.com>
2024-01-01 16:39:07 +0100

    added logging, resilience4j and validation dependencies

--- src/main/java/at/technikum/project/CleanCodeProjectApplication.java
+++ src/main/java/at/technikum/project/CleanCodeProjectApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CleanCodeProjectApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(CleanCodeProjectApplication.class, args);
 	}
