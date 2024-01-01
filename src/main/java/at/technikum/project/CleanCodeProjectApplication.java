ca5fc70bdeea5b9cc5a9643781c1b05b59397d0f
LeonKattendick <leon.kattendick@gmail.com>
2024-01-01 16:18:42 +0100

    init

---
+++ src/main/java/at/technikum/project/CleanCodeProjectApplication.java
@@ -0,0 +1,11 @@
+package at.technikum.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanCodeProjectApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CleanCodeProjectApplication.class, args);
+	}
+}
